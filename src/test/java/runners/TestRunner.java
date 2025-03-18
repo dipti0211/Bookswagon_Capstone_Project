0b010a44be69cf21d5f13fe2612cdfc0f50d28ff
dipti0211 <diptimayee0021@gmail.com>
2025-03-18 16:24:39 +0530

    Updated json file

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,15 +1,17 @@
 package runners;
 
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src/test/resources/features",
-		glue = {"stepdefinitions"},
-		plugin = {"pretty", "json:target/cucumber-reports-json/Cucumber.json"},
-		//plugin = {"pretty", "html:target/cucumber-reports.html"},
-		monochrome = true
-		)
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions"},
+        plugin = {
+                "pretty",
+                "json:target/cucumber-reports-json/Cucumber.json", 
+                "html:target/cucumber-reports-html/index.html"
+        },
+        monochrome = true
+)
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
